08c6b6d6921e49ea7ecec05d07585b5e0fd3362b
Junior Miqueletti <juniormiqueletti@gmail.com>
2017-02-11 14:13:22 -0200

    Fixed Locale resolver

--- src/main/java/com/juniormiqueletti/SecuritiesApplication.java
+++ src/main/java/com/juniormiqueletti/SecuritiesApplication.java
@@ -1,7 +1,11 @@
 package com.juniormiqueletti;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
 public class SecuritiesApplication {
@@ -9,4 +13,10 @@ public class SecuritiesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SecuritiesApplication.class, args);
 	}
+	
+	@Bean
+	public FixedLocaleResolver localeResolver(){
+		return new FixedLocaleResolver(new Locale("pt","BR"));
+	}
+	
 }
